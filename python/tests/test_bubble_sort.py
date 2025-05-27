0e2169821fcd04e3f167a8858201faead6e0d3b3
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-05-27 20:58:40 -0300

    Add tests!

---
+++ python/tests/test_bubble_sort.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+import unittest
+from algorithms.sort.bubble import bubble_sort
+
+
+class TestBubbleSort(unittest.TestCase):
+    def test_bubble_sort(self):
+        nums = [7, 4, 9, 2, 1, 2]
+        bubble_sort(nums)
+        self.assertEqual([1, 2, 2, 4, 7, 9], nums)
+
+
+if __name__ == "__main__":
+    unittest.main()
