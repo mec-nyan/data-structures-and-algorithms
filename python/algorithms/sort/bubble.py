741f2088058e49d362737162136612075a5597e3
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-05-27 20:34:46 -0300

    Bubble sort in Python!

---
+++ python/algorithms/sort/bubble.py
@@ -0,0 +1,10 @@
+def bubble_sort(nums: list[int]):
+    n = len(nums)
+    for i in range(n):
+        swapped = False
+        for j in range(n - (i + 1)):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+                swapped = True
+        if not swapped:
+            break
---
+++ python/demos/bubble_sort.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+from algorithms.sort.bubble import bubble_sort
+
+nums = [234, 54, 11, 67, 7, 5, 42, 7]
+
+print("Before sorting:", nums)
+
+bubble_sort(nums)
+
+print("After sorting: ", nums)
