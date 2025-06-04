3455cbcf37a0c88444a1cc370ca2484ede8233a2
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-06-04 19:15:20 -0300

    Balanced parens in Python.

---
+++ python/demos/balanced_parens.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Check if a series of parenthesis is balanced."""
+
+from data_structures.stack.stack import Stack
+
+
+def is_balanced(s: str) -> bool:
+    '''Check if parenthesis are balanced.'''
+
+    stk = Stack()
+    balanced = True
+
+    i = 0
+    while i < len(s) and balanced:
+        if s[i] == "(":
+            stk.push(s[i])
+        elif s[i] == ")":
+            if not stk.is_empty():
+                stk.pop()
+            else:
+                balanced = False
+        i += 1
+
+    return stk.size() == 0
+
+
+if __name__ == "__main__":
+    s1 = "((()())())"
+    s2 = "((()(()())"
+
+    print(f"{s1} - balanced? {is_balanced(s1)}")
+    print(f"{s2} - balanced? {is_balanced(s2)}")
