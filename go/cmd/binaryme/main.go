3c4c1444cc14a020e42ed9beb3a84960223b6b71
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-06-05 21:19:30 -0300

    Decimal to binary in Go.

---
+++ go/cmd/binaryme/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+
+	"github.com/mec-nyan/data-structures-and-algorithms/go/pkg/stack"
+)
+
+// binaryme converts a decimal number to its binary representation.
+
+
+func main() {
+	for i := 0; i < 16; i++ {
+		bin, _ := toBin(i)
+		fmt.Printf("dec: %2d, bin: %04s\n", i, bin)
+	}
+}
+
+func toBin(dec int) (string, error) {
+	if dec < 0 {
+		return "", errors.New("Only positives for now!")
+	}
+
+	if dec == 0 {
+		return "0", nil
+	}
+
+	stk := stack.New[int]()
+
+	for dec > 0 {
+		stk.Push(dec % 2)
+		dec /= 2
+	}
+
+	var out string
+
+	for !stk.IsEmpty() {
+		next := stk.Pop().(int)
+		out += strconv.Itoa(next)
+	}
+
+	return out, nil
+}
